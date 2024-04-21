399e906fe2d0a0739a6372e31f2466b92129cb5a
kermitlafrog61 <islamchikalybaev@gmail.com>
2024-04-22 00:29:03 +0600

    backups

--- backup.sh
+++ backup.sh
@@ -4,6 +4,6 @@ compose_name=$(basename "$(pwd)")
 volumes="${compose_name}_media ${compose_name}_postgres_data"
 
 for volume in ${volumes}; do
-    volume_directory=$(docker volume inspect "${volume}" | jq '.[0].Mountpoint')
+    volume_directory=$(sudo docker inspect $volume | grep Mountpoint | awk '{ print $2 }')
     tar cvf backup/${volume}.tar -C $volume_directory .
 done
