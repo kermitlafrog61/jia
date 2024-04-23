6edc895cefad2d2d2a1085d5c1b56bdb38ff4331
kermitlafrog61 <islamchikalybaev@gmail.com>
2024-04-23 19:09:12 +0600

    news corrected

--- src/apps/news/mixins.py
+++ src/apps/news/mixins.py
@@ -5,6 +5,7 @@ class NewsViewMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['related_news'] = models.News.objects.all()
+        context['news_tag'] = []
         for tag in models.NewsTag.objects.all():
             news_count = models.News.objects.filter(tag=tag).count()
             context['news_tag'].append(
